6bc21a1f1468e7f7be4545f2f3a64d9cd3570b13
dgarciaortiz94 <dgarciaortiz94@gmail.com>
2023-01-18 01:02:42 +0100

    add api methods to leads controller

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -2,31 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
+use App\Models\LeadStatus;
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LeadController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        
+        $queryBuilder = DB::table('leads');
+
+        foreach ($request->toArray() as $key => $value) {
+            $queryBuilder->orWhere($key, '=', $value);
+        }
+
+        $leads = $queryBuilder->get();
+
+        return $leads[0];
     }
 
-    public function new()
+    public function store(Request $request)
     {
-        
+        $lead = new Lead();
+
+        $lead->status = $request->status;
+        $lead->title = $request->title;
+        $lead->created_at = new DateTime();
+
+        $lead->save();
+
+        return $lead;
     }
 
-    public function show()
+    public function show(int $id)
     {
-        
+        $lead = Lead::find($id);
+
+        return $lead;
     }
 
-    public function edit()
+    public function update(Request $request, int $id)
     {
+        $lead = Lead::find($id);
+ 
+        $lead->status = $request->status;
+        $lead->title = $request->title;
         
+        $lead->save();
+
+        return $lead;
     }
 
-    public function delete()
+    public function destroy(int $id)
     {
         
     }

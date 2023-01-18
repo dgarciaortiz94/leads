74b34aa8f0eef4f72eda90be395dc5532a471786
dgarciaortiz94 <dgarciaortiz94@gmail.com>
2023-01-18 01:01:53 +0100

    add leads api endpoints to routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LeadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::controller(LeadController::class)->group(function () {
+    Route::get('/leads', 'index');
+    Route::post('/leads', 'store');
+    Route::get('/leads/{id}', 'show');
+    Route::patch('/leads/{id}', 'update');
+    Route::delete('/leads/{id}', 'destroy');
 });

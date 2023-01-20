18c88461eeaa03d93da3796a2f803e8d59ec63a1
dgarciaortiz94 <dgarciaortiz94@gmail.com>
2023-01-20 18:55:41 +0100

    document the lead status CRUDs

--- app/Http/Controllers/LeadStatusController.php
+++ app/Http/Controllers/LeadStatusController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LeadStatus;
 use Illuminate\Http\Request;
 
+/**
+ * This class handle lead status api CRUD.
+ *
+ * @author Diego García <dgarciaortiz94@gmail.com>
+ * @license https://es.wikipedia.org/wiki/GNU_General_Public_License
+ */ 
 class LeadStatusController extends Controller
 {
-    //
+    /**
+     * Return all lead status.
+     * 
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        $leadStatus = LeadStatus::all();
+
+        return $leadStatus;
+    }
 }

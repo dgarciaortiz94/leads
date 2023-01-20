1b23ec183a76f5f3f975fcbf0cf20209f4a460b3
dgarciaortiz94 <dgarciaortiz94@gmail.com>
2023-01-20 18:55:28 +0100

    document the leads CRUDs

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -4,25 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Models\Lead;
 use App\Models\LeadStatus;
+use App\Services\BracketsParser;
+use App\Services\LeadsMailer;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * This class handle leads api CRUD.
+ *
+ * @author Diego García <dgarciaortiz94@gmail.com>
+ * @license https://es.wikipedia.org/wiki/GNU_General_Public_License
+ */ 
 class LeadController extends Controller
 {
+    /**
+     * Filter and return leads.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * 
+     * @return JsonResponse
+     */
     public function index(Request $request)
     {
         $queryBuilder = DB::table('leads');
 
-        foreach ($request->toArray() as $key => $value) {
-            $queryBuilder->orWhere($key, '=', $value);
+        foreach ($request->toArray() as $property => $filter) {
+            foreach ($filter as $operator => $value) {
+                if (BracketsParser::parse($operator) == 'AND') {
+                    $queryBuilder->where($property, BracketsParser::parse($operator), $value);
+                } else {
+                    $queryBuilder->orWhere($property, BracketsParser::parse($operator), $value);
+                }
+            }
         }
 
-        $leads = $queryBuilder->get();
+        $leads = $queryBuilder->orderBy('created_at', 'desc')->get();
 
-        return $leads[0];
+        return $leads[0]->lead;
     }
 
+    /**
+     * Store a lead.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * 
+     * @return JsonResponse
+     */
     public function store(Request $request)
     {
         $lead = new Lead();
@@ -36,6 +64,13 @@ class LeadController extends Controller
         return $lead;
     }
 
+    /**
+     * Show a lead.
+     *
+     * @param  int  $id
+     * 
+     * @return JsonResponse
+     */
     public function show(int $id)
     {
         $lead = Lead::find($id);
@@ -43,20 +78,33 @@ class LeadController extends Controller
         return $lead;
     }
 
-    public function update(Request $request, int $id)
-    {
-        $lead = Lead::find($id);
- 
-        $lead->status = $request->status;
-        $lead->title = $request->title;
-        
-        $lead->save();
 
-        return $lead;
-    }
-
-    public function destroy(int $id)
+    /**
+     * Close leads with 6 months old
+     * 
+     * @return JsonResponse
+     */
+    public function closeLeads()
     {
-        
+        $currentDate = new DateTime();
+        $formattedCurrentDate = $currentDate->format('Y-m-d H:i:s');
+
+        $sixMonthAgo = date("Y-m-d", strtotime($formattedCurrentDate . "- 6 month")); 
+
+        $queryBuilder = Lead::where('created_at', '<', $sixMonthAgo)
+                            ->whereNull('closed_at');
+
+        $leads = $queryBuilder->get();
+
+        $queryBuilder->update(['closed_at' => $formattedCurrentDate]);
+
+        /*
+         * I cant test and send emails because of I need a mail server.
+         */
+        // foreach ($leads as $lead) {
+        //     LeadsMailer::basic_email($lead);
+        // }
+
+        return $leads;
     }
 }
